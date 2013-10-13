cde301b280a000a3c69381f008ea452fb59d214b
Ivan Iljkic <i.iljkic@gmail.com>
2013-10-13 16:54:16 +0200

    unused formats removed

--- src/main/java/ch/uzh/csg/utp4j/channels/impl/conn/ConnectionTimeOutRunnable.java
+++ src/main/java/ch/uzh/csg/utp4j/channels/impl/conn/ConnectionTimeOutRunnable.java
@@ -1,14 +1,8 @@
 package ch.uzh.csg.utp4j.channels.impl.conn;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
 import java.util.concurrent.locks.ReentrantLock;
 
-import ch.uzh.csg.utp4j.channels.UtpSocketState;
 import ch.uzh.csg.utp4j.channels.impl.UtpSocketChannelImpl;
-import ch.uzh.csg.utp4j.channels.impl.alg.UtpAlgConfiguration;
 import ch.uzh.csg.utp4j.data.UtpPacket;
 
 public class ConnectionTimeOutRunnable implements Runnable {
